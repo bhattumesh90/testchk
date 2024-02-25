3afa7a725e9e2c19e01bb2e598e4e4407e2a11fd
bhattumesh90 <bhattumesh90@gmail.com>
2024-02-25 14:30:27 +0530

    25-02-2024 initial commit

---
+++ src/main/java/com/example/demo/TestchkApplication.java
@@ -0,0 +1,16 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.lang;
+
+
+@SpringBootApplication
+public class TestchkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestchkApplication.class, args);
+	}
+
+	System.out.println("Test checking condition");
+}
